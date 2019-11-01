0271bbbf4976fb6052dba993320a54f15461a749
DOUKSIEH <douksieha@gmail.com>
2019-11-01 11:38:54 +0100

    modification css et adcontroller

---
+++ src/Controller/AdController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Ad;
+use App\Repository\AdRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class AdController extends AbstractController
+{
+    /**
+    * @Route("/ads", name="ads_index")
+    */
+    public function index(AdRepository $repo)
+    {
+        $ads = $repo->findAll();
+        return $this->render('ad/index.html.twig', [
+            'ads' => $ads
+        ]);
+    }
+    
+    /**
+     * Permet de créer une annonce
+     *
+     * @Route("/ads/new", name="ads_create")
+     * 
+     * @return Response
+     */
+    public function create(Request $request, ObjectManager $manager)
+    {
+         $ad = new Ad();
+        $form = $this->createForm(AdType::class, $ad);
+        $form->handleRequest($request);
+        
+        return $this->render('ad/new.html.twig');
+    }
+    /**
+     * Permet d'afficher une seule annonce
+     *
+     * @Route("/ads/{slug}", name="ads_show")
+     * 
+     * @return Response
+     */
+    public function show(Ad $ad)
+    {
+        return $this->render('ad/show.html.twig', [
+            'ad' => $ad
+        ]);
+    }
+    
+}
